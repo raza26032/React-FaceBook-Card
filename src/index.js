b2a0bf7545851d3f6bc6ed914fddf399a737be7c
Ahmed Raza <74352699+raza26032@users.noreply.github.com>
2021-01-29 16:32:50 +0500

    ok

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function MediaCard({ profilePic, title, time, body, imgUrl }) {
+function MediaCard({ profilePic, title, body, imgUrl }) {
+
+  const [value, setValue] = React.useState(true);
+
+  function cllick() {
+    setValue((previousValue) => (!previousValue))
+  }
   return <div>
+    <input type="checkbox" onClick={cllick}></input>
     <div className="card-container">
-      <img className="round" src={profilePic} />
-      <div className="name"><a href="#"><b>{title}</b></a></div>
-      <div className="content">
-        <p>{body}</p>
-      </div>
-      <div className="square">
-        <img className="square-post" src={imgUrl} />
+      <div className={(value === true) ? "Dark" : "Bright"}>
+        <img className="round" src={profilePic} />
+        <div className="name"><a><b>{title}</b></a></div>
+        <div className="content">
+          <p>{body}</p>
+        </div>
+        <div className="square">
+          <img className="square-post" src={imgUrl} />
+        </div>
       </div>
     </div>
   </div>
@@ -22,14 +31,6 @@ ReactDOM.render(
     <MediaCard profilePic="https://pbs.twimg.com/profile_images/1162267138842755075/zxLifsgg.jpg"
       title="@ImranKhan" body="Imran Ahmed Khan Niazi HI PP is the 22nd and current Prime Minister of Pakistan and the chairman of the Pakistan Tehreek-e-Insaf. Before entering politics, Khan was an international cricketer and captain of the Pakistan national cricket team, which he led to victory in the 1992 Cricket World Cup."
       imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu5SmSUMqFF_TkfroF-Ll4I3boH-juOhHegg&usqp=CAU" />
-      <br />
-      <hr />
-      <br />
-    <MediaCard profilePic="https://upload.wikimedia.org/wikipedia/commons/4/46/PrimeMinisterNawazSharif.jpg"
-      title="@NawazSharef" body="Mian Muhammad Nawaz Sharif is a Pakistani businessman and politician who served as the prime minister of Pakistan for three non-consecutive terms. Sharif is the longest-serving prime minister of Pakistan having served a total of more than 9 years."
-      imgUrl="https://incpak.com/wp-content/uploads/2018/05/PMLN-Facebook-Cover.jpg" />
-      <br />
-      <br />
   </>,
   document.getElementById('root')
 );
